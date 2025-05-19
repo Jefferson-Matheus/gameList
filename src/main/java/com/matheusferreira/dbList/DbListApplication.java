cc76ea3966eb1a55251ac45f9edbe249a52946da
Jefferson Ferreira <jeffersonmatheusu@gmail.com>
2025-05-19 10:00:56 -0300

    :tada: init: Projeto Inicializado

---
+++ src/main/java/com/matheusferreira/dbList/DbListApplication.java
@@ -0,0 +1,13 @@
+package com.matheusferreira.dbList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusferreira/dbList/DbListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusferreira.dbList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
