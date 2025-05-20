d050a0e9657519e72658207f170d90689fbdc74e
Jefferson Ferreira <jeffersonmatheusu@gmail.com>
2025-05-20 20:44:59 -0300

    :sparkles: feat: Criando Entidadde De Jogo Para O Banco De Dados

--- src/main/java/com/matheusferreira/dbList/entities/Game.java
+++ src/main/java/com/matheusferreira/dbList/entities/Game.java
@@ -10,20 +10,29 @@ public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    private String title;
+
     @Column(name = "game_year")
     private int year;
+
     private String genre;
     private String platforms;
     private double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
     }
 
-    public Game(long id, int year, String genre, String platforms, double score, String imgUrl, String shortDescription, String longDescription) {
+    public Game(long id, String title, int year, String genre, String platforms, double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
+        this.title = title;
         this.year = year;
         this.genre = genre;
         this.platforms = platforms;
