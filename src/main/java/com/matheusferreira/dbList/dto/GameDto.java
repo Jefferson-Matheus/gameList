5f3e0cda6d0a6e5f4c21e62be34d90fa21173345
Jefferson Ferreira <jeffersonmatheusu@gmail.com>
2025-05-22 10:08:55 -0300

    :sparkles: feat: Criação do Metodo findById

---
+++ src/main/java/com/matheusferreira/dbList/dto/GameDto.java
@@ -0,0 +1,100 @@
+package com.matheusferreira.dbList.dto;
+
+import com.matheusferreira.dbList.entities.Game;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.beans.BeanUtils;
+
+public class GameDto {
+
+    private long id;
+    private String title;
+    private int year;
+    private String genre;
+    private String platforms;
+    private double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public GameDto() {
+    }
+
+    public GameDto(Game gameEntity) {
+        BeanUtils.copyProperties(gameEntity, this);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+}
--- src/main/java/com/matheusferreira/dbList/services/GameService.java
+++ src/main/java/com/matheusferreira/dbList/services/GameService.java
@@ -1,10 +1,12 @@
 package com.matheusferreira.dbList.services;
 
+import com.matheusferreira.dbList.dto.GameDto;
 import com.matheusferreira.dbList.dto.GameMinDto;
 import com.matheusferreira.dbList.entities.Game;
 import com.matheusferreira.dbList.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,11 +16,21 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDto> findAll(){
         List<Game> gamesResult = gameRepository.findAll();
 
         List<GameMinDto> gameMinimalInformations = gamesResult.stream().map(x -> new GameMinDto(x)).toList();
 
         return  gameMinimalInformations;
+    }
+
+    @Transactional(readOnly = true)
+    public GameDto findById(Long id){
+        Game singleGameResult = gameRepository.findById(id).get();
+
+        GameDto gameDto = new GameDto(singleGameResult);
+
+        return gameDto;
     }
 }
