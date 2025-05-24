d3e13f9dc7b8ea70b9a41603c0b3d4132f77c943
Jefferson Ferreira <jeffersonmatheusu@gmail.com>
2025-05-24 11:37:04 -0300

    :sparkles: feat: Criação Do Metodo De Reposicionamento De Jogos Em Suas Categorias

---
+++ src/main/java/com/matheusferreira/dbList/dto/ReplacementDto.java
@@ -0,0 +1,24 @@
+package com.matheusferreira.dbList.dto;
+
+public class ReplacementDto {
+
+    private int sourceIndex;
+
+    private int destinationIndex;
+
+    public int getSourceIndex() {
+        return sourceIndex;
+    }
+
+    public void setSourceIndex(int sourceIndex) {
+        this.sourceIndex = sourceIndex;
+    }
+
+    public int getDestinationIndex() {
+        return destinationIndex;
+    }
+
+    public void setDestinationIndex(int destinationIndex) {
+        this.destinationIndex = destinationIndex;
+    }
+}
--- src/main/java/com/matheusferreira/dbList/services/GameListService.java
+++ src/main/java/com/matheusferreira/dbList/services/GameListService.java
@@ -2,7 +2,9 @@ package com.matheusferreira.dbList.services;
 
 import com.matheusferreira.dbList.dto.GameListDto;
 import com.matheusferreira.dbList.entities.GameList;
+import com.matheusferreira.dbList.projections.GameMinProjection;
 import com.matheusferreira.dbList.repositories.GameListRepository;
+import com.matheusferreira.dbList.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +17,9 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDto> findAllList(){
        List<GameList> gameListResult = gameListRepository.findAll();
@@ -22,5 +27,17 @@ public class GameListService {
        List<GameListDto> gameList = gameListResult.stream().map(x -> new GameListDto(x)).toList();
 
        return gameList;
+    }
+
+    @Transactional
+    public void replaceGamesONList(Long listId, int sourceIndex, int destinationIndex) {
+        List<GameMinProjection> listOfGamesBycategory = gameRepository.searchByList(listId);
+        GameMinProjection objectRemoved = listOfGamesBycategory.remove(sourceIndex);
+        listOfGamesBycategory.add(destinationIndex, objectRemoved);
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId, listOfGamesBycategory.get(i).getId(), i);
+        }
     }
 }
